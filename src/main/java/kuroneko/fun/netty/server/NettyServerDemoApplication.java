0fdd2890011afdee918296fb4fb67b117f35e3d7
kuroneko <964621334@qq.com>
2021-04-02 17:59:32 +0800

    add_rocketmq

--- src/main/java/kuroneko/fun/netty/server/NettyServerDemoApplication.java
+++ src/main/java/kuroneko/fun/netty/server/NettyServerDemoApplication.java
@@ -1,16 +1,29 @@
 package kuroneko.fun.netty.server;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
-public class NettyServerDemoApplication {
+@Slf4j
+public class NettyServerDemoApplication  {
 
 
     public static void main(String[] args) {
 
         SpringApplication.run(NettyServerDemoApplication.class, args);
+    }
+    @Service
+    @RocketMQMessageListener(topic = "TopicTest", consumerGroup = "my-consumer_TopicTest")
+     class MyConsumer implements RocketMQListener<String> {
+        @Override
+        public void onMessage(String message) {
+            log.info("received message: " + message);
+        }
     }
 
 }
--- src/main/java/kuroneko/fun/netty/server/runner/ServerRunner.java
+++ src/main/java/kuroneko/fun/netty/server/runner/ServerRunner.java
@@ -4,8 +4,10 @@ import io.netty.channel.ChannelFuture;
 import javax.annotation.Resource;
 import kuroneko.fun.netty.server.config.ServerConfig;
 import kuroneko.fun.netty.server.utils.ServerBootStrap;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,16 +21,18 @@ public class ServerRunner implements ApplicationRunner {
   @Resource
   ServerBootStrap serverBootStrap;
 
-
   @Override
   public void run(ApplicationArguments args)  {
     ChannelFuture future = serverBootStrap.start(serverConfig.getIp(), serverConfig.getPort());
-    Runtime.getRuntime().addShutdownHook(new Thread(){
+    Runtime.getRuntime().addShutdownHook(
+        new Thread(){
       @Override
       public void run(){
         serverBootStrap.destroy();
       }
     });
     future.channel().closeFuture().syncUninterruptibly();
+
+
   }
 }
